89fc587531806fc06d40d22c58965ee4336c38de
d1ew1thmem0rtal <d1ew1thmem0rtal@gmail.com>
2025-05-06 19:31:27 +0400

    feat: add difference of numbers

--- github_test/Program.cs
+++ github_test/Program.cs
@@ -4,13 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Введите первое число:");
-        var firstNumber = Convert.ToDouble(Console.ReadLine());
+        while (true)
+        {
+            Console.WriteLine("Введите первое число:");
+            var firstNumber = Convert.ToDouble(Console.ReadLine());
         
-        Console.WriteLine("Введите второе число:");
-        var secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Выберите команду: \n" +
+                              $"1. Сложить числа \n" +
+                              $"2. Вычесть числа \n");
 
-        var sum = firstNumber + secondNumber;
-        Console.WriteLine("Результат сложения " + sum);
+            var choice = Convert.ToInt32(Console.ReadLine());
+        
+            Console.WriteLine("Введите второе число:");
+            var secondNumber = Convert.ToDouble(Console.ReadLine());
+            
+            switch (choice)
+            {
+                case 1:
+                {
+                    var sum = firstNumber + secondNumber;
+                    Console.WriteLine("Результат сложения " + sum);
+                    break;
+                }
+                case 2:
+                {
+                    var difference = firstNumber - secondNumber;
+                    Console.WriteLine("Результат вычитания " + difference);
+                    break;
+                }
+            }
+        }
     }
 }
